d371aaf014b0d686a096ab53ceb9f03d2e4c14ee
GabrielaCorona19 <70930147+GabrielaCorona19@users.noreply.github.com>
2020-12-03 10:18:04 -0600

    Update mutex.java

--- mutex.java
+++ mutex.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package condicionescompetencias;
 
-
-
-/**
- *
- * @author edgar
- */
 public class mutex {
     private boolean pase;
     mutex(boolean pase){
